11eaa1e8d8d001574da43c79ee265cb9aafe8226
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-02-20 20:31:16 +0530

    ✨ Home Page Title Added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,60 @@
+import { Container, makeStyles, Typography } from '@material-ui/core'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import Carousel from './Carousel'
+
+const useStyles = makeStyles(() => ({
+    banner : {
+        backgroundImage: 'url(./banner2.jpg)',
+    },
+    bannerContext : {
+        height: 400,
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: 25,
+        justifyContent: "space-around",
+    },
+    tagline: {
+        display: "flex",
+        height: "40%",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+      },
+}))
+
+const Banner = () => {
+    const classes = useStyles();
+    const history = useHistory();
+    return (
+        <div className={classes.banner}>
+            <Container className={classes.bannerContext}>
+                <div className={classes.tagline}>
+                    <Typography
+                        variant="h2"
+                        style={{
+                        fontWeight: "bold",
+                        marginBottom: 15,
+                        fontFamily: "Montserrat",
+                        }}
+                    >
+                        Crypto Hunter
+                    </Typography>
+                    <Typography
+                        variant="subtitle2"
+                        style={{
+                        color: "darkgrey",
+                        textTransform: "capitalize",
+                        fontFamily: "Montserrat",
+                        }}
+                    >
+                        Get all the Info regarding your favorite Crypto Currency
+                    </Typography>
+                </div>
+                <Carousel />
+            </Container>
+        </div>
+  )
+}
+
+export default Banner;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Banner from '../Components/Banner/Banner'
 
 const HomePage = () => {
   return (
-    <div>
-      Homepage
-    </div>
+    <>
+      <Banner />
+    </>
   )
 }
 
