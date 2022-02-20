7b670ece3a667fec9b5645dfe7ca1c58db23207b
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-02-20 21:10:30 +0530

    ✨ Added Trending Coins Carousel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import CryptoContext from './CryptoContext'
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
