52bf762942b13a848e5dce3159ef2807965133cd
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-02-22 16:58:56 +0530

    ✨ onAuthStateChanged Function

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -24,7 +24,7 @@ const darkTheme = createTheme({
 const Header = () => {
     const classes = useStyles();
     const history = useHistory();
-    const { currency, setCurrency } = CryptoState();
+    const { currency, setCurrency, user } = CryptoState();
     console.log(currency);
 
   return (
@@ -43,7 +43,7 @@ const Header = () => {
                         <MenuItem value={'USD'}>USD</MenuItem>
                         <MenuItem value={'INR'}>INR</MenuItem>
                     </Select>
-                    <AuthModal />
+                    {user ? "Logout" : <AuthModal />}
                 </Toolbar>
             </Container>
         </AppBar>
